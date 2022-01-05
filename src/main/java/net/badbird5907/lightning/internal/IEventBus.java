a739944bfdee22182a3bca9a174bd6029775be34
Badbird-5907 <50347938+Badbird-5907@users.noreply.github.com>
2022-01-05 13:37:23 -0500

    javadocs

--- src/main/java/net/badbird5907/lightning/internal/IEventBus.java
+++ src/main/java/net/badbird5907/lightning/internal/IEventBus.java
@@ -8,24 +8,56 @@ import java.util.List;
 import java.util.Map;
 
 public interface IEventBus {
+    /**
+     * Register a listener class
+     * This will only register static methods
+     * @param event
+     */
     void register(Class<?> event);
 
+    /**
+     * Register a listener class instance
+     * This will register both static and instance methods
+     * @param listener
+     */
     void register(Object listener);
 
+    /**
+     * Register a EventHandler annotation, method and instance
+     * @param handler The EventHandler annotation
+     * @param method The method to register
+     * @param instance The instance to register
+     */
     void register(EventHandler handler, Method method, Object instance);
 
     default void register(EventHandler handler, Method method) {
         register(handler, method, null);
     }
 
+    /**
+     * Get a map of all registered listeners
+     * @return
+     */
     Map<Class<? extends Event>, List<EventInfo>> getListeners();
 
+    /**
+     * call an event
+     * @param event
+     */
     void call(Event event);
 
+    /**
+     * call an event
+     * @param event
+     */
     default void callEvent(Event event) {
         call(event);
     }
 
+    /**
+     * call an event
+     * @param event
+     */
     default void post(Event event) {
         call(event);
     }
